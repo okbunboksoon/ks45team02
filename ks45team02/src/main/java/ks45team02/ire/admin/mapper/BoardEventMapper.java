ebf5452c0cf530401b6cf374a57b169a06ac7e96
임수락 <isk9612@naver.com>
2022-12-30 15:02:28 +0900

    [teamproject ire] author: isr 변경사항: 이벤트 리스트 조회 일부 완성 변경시간: 12월 30일 15시 02분

--- ks45team02/src/main/java/ks45team02/ire/admin/mapper/BoardEventMapper.java
+++ ks45team02/src/main/java/ks45team02/ire/admin/mapper/BoardEventMapper.java
@@ -1,5 +1,12 @@
 package ks45team02.ire.admin.mapper;
 
+import ks45team02.ire.admin.dto.BoardEvent;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
 public interface BoardEventMapper {
 
+    List<BoardEvent> getBoardEventList();
 }
