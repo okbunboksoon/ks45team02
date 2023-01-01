cf902833b9c00a83400d751b4f7c2c1029152abf
cossw <cossw@DESKTOP-UI7EGP1>
2023-01-01 21:50:35 +0900

    [teamproject ire] author: lym 변경사항: FAQ 등록, 리스트 html 수정 변경시간: 1월 1일 21시 50분

--- ks45team02/src/main/java/ks45team02/ire/admin/controller/BoardFAQController.java
+++ ks45team02/src/main/java/ks45team02/ire/admin/controller/BoardFAQController.java
@@ -1,19 +1,60 @@
 
 package ks45team02.ire.admin.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ks45team02.ire.admin.dto.BoardFAQ;
+import ks45team02.ire.admin.dto.BoardNotice;
+import ks45team02.ire.admin.service.BoardFAQService;
+import ks45team02.ire.admin.service.BoardNoticeService;
 
 @Controller
 @RequestMapping("/admin")
 public class BoardFAQController {
+	
+	private static final Logger log = LoggerFactory.getLogger(BoardNoticeController.class);
 
+	
+	private final BoardFAQService boardFAQService;
+	
+	public BoardFAQController(BoardFAQService boardFAQService) {
+		
+		this.boardFAQService = boardFAQService;
+		
+	}
+	
+	/**
+	 * FAQ 등록 화면
+	 * @return admin/board/boardAddNotice
+	 */
 	@GetMapping("/addBoardFAQ")
 	public String addBoardFAQ(Model model) {
 		
+		model.addAttribute("title", "FAQ 등록");
+		
 		return "admin/board/boardAddFAQ";
+	}
+	
+	/**
+	 * FAQ 등록
+	 * @param boardFAQ
+	 * @return
+	 */
+	
+	@PostMapping("/addBoardFAQ")
+	public String addBoardFAQ(BoardFAQ boardFAQ) {
+		
+		log.info("FAQ 정보 :{}", boardFAQ);
+		
+		boardFAQService.addFAQ(boardFAQ);
+		
+		return "redirect:/admin/listBoardFAQ";
 	}
 	
 	@GetMapping("/deleteBoardFAQ")
