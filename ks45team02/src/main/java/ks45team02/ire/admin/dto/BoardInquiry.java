b6a94edcd461fc12faacbb1be41e35329876890f
JaeBeomk <kimjm3435@naver.com>
2023-01-04 12:07:50 +0900

    [teamproject ire] author: KJB 변경사항: 1:1문의 조회, 회원 로그인내역조회 변경시간: 1월 4일 12시 07분

---
+++ ks45team02/src/main/java/ks45team02/ire/admin/dto/BoardInquiry.java
@@ -0,0 +1,101 @@
+package ks45team02.ire.admin.dto;
+
+public class BoardInquiry {
+
+    private String inquiryCode;
+    private String userId;
+    private String inquiryCate;
+    private String inquiryTitle;
+    private String inquiryContents;
+    private String inquiryFile;
+    private String inquiryRegDate;
+    private int inquiryReRef;
+    private int inquiryReLev;
+
+    public String getInquiryCode() {
+        return inquiryCode;
+    }
+
+    public void setInquiryCode(String inquiryCode) {
+        this.inquiryCode = inquiryCode;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getInquiryCate() {
+        return inquiryCate;
+    }
+
+    public void setInquiryCate(String inquiryCate) {
+        this.inquiryCate = inquiryCate;
+    }
+
+    public String getInquiryTitle() {
+        return inquiryTitle;
+    }
+
+    public void setInquiryTitle(String inquiryTitle) {
+        this.inquiryTitle = inquiryTitle;
+    }
+
+    public String getInquiryContents() {
+        return inquiryContents;
+    }
+
+    public void setInquiryContents(String inquiryContents) {
+        this.inquiryContents = inquiryContents;
+    }
+
+    public String getInquiryFile() {
+        return inquiryFile;
+    }
+
+    public void setInquiryFile(String inquiryFile) {
+        this.inquiryFile = inquiryFile;
+    }
+
+    public String getInquiryRegDate() {
+        return inquiryRegDate;
+    }
+
+    public void setInquiryRegDate(String inquiryRegDate) {
+        this.inquiryRegDate = inquiryRegDate;
+    }
+
+    public int getInquiryReRef() {
+        return inquiryReRef;
+    }
+
+    public void setInquiryReRef(int inquiryReRef) {
+        this.inquiryReRef = inquiryReRef;
+    }
+
+    public int getInquiryReLev() {
+        return inquiryReLev;
+    }
+
+    public void setInquiryReLev(int inquiryReLev) {
+        this.inquiryReLev = inquiryReLev;
+    }
+
+    @Override
+    public String toString() {
+        return "BoardInquiry{" +
+                "inquiryCode='" + inquiryCode + '\'' +
+                ", userId='" + userId + '\'' +
+                ", inquiryCate='" + inquiryCate + '\'' +
+                ", inquiryTitle='" + inquiryTitle + '\'' +
+                ", inquiryContents='" + inquiryContents + '\'' +
+                ", inquiryFile='" + inquiryFile + '\'' +
+                ", inquiryRegDate='" + inquiryRegDate + '\'' +
+                ", inquiryReRef=" + inquiryReRef +
+                ", inquiryReLev=" + inquiryReLev +
+                '}';
+    }
+}
---
+++ ks45team02/src/main/java/ks45team02/ire/admin/service/BoardInquiryService.java
@@ -0,0 +1,32 @@
+package ks45team02.ire.admin.service;
+
+import ks45team02.ire.admin.dto.BoardInquiry;
+import ks45team02.ire.admin.mapper.BoardInquiryMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class BoardInquiryService {
+    private static final Logger log = LoggerFactory.getLogger(BoardNoticeService.class);
+    private final BoardInquiryMapper boardInquiryMapper ;
+
+    public BoardInquiryService(BoardInquiryMapper boardInquiryMapper) {
+        this.boardInquiryMapper = boardInquiryMapper;
+    }
+
+    public List<BoardInquiry>boardInquiryList(){
+        // 1:1문의 전체 조회
+        List<BoardInquiry> boardInquiryList=boardInquiryMapper.listInquiryList();
+        return boardInquiryList;
+    }
+    public int addInquiry(BoardInquiry boardInquiry){
+        return boardInquiryMapper.addInquiry(boardInquiry);
+    }
+
+
+}
