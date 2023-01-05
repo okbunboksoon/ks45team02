99b0e0f399aa0d458b92835f88aa374a0c42652f
ksmart <ksmart@DESKTOP-LIJ9H19>
2023-01-05 14:41:54 +0900

    [teamproject ire] author: kmk 변경사항: buynow dto 생성 변경시간: 1월 5일 14시 41분

---
+++ ks45team02/src/main/java/ks45team02/ire/admin/dto/Buynow.java
@@ -0,0 +1,119 @@
+package ks45team02.ire.admin.dto;
+
+public class Buynow {
+	private String buynowCode;
+	private String userId;
+	private String goodsCode;
+	private String goodsName;
+	private String categoryMediumCode;
+	private String categoryMediumName;
+	private String goodsPrice;
+	private String buynowAmount;
+	private String expectedAmount;
+	private String usePoint;
+	private String finalPaymentAmount;
+	private String regDate;
+	public String getBuynowCode() {
+		return buynowCode;
+	}
+	public void setBuynowCode(String buynowCode) {
+		this.buynowCode = buynowCode;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getGoodsCode() {
+		return goodsCode;
+	}
+	public void setGoodsCode(String goodsCode) {
+		this.goodsCode = goodsCode;
+	}
+	public String getGoodsName() {
+		return goodsName;
+	}
+	public void setGoodsName(String goodsName) {
+		this.goodsName = goodsName;
+	}
+	public String getCategoryMediumCode() {
+		return categoryMediumCode;
+	}
+	public void setCategoryMediumCode(String categoryMediumCode) {
+		this.categoryMediumCode = categoryMediumCode;
+	}
+	public String getCategoryMediumName() {
+		return categoryMediumName;
+	}
+	public void setCategoryMediumName(String categoryMediumName) {
+		this.categoryMediumName = categoryMediumName;
+	}
+	public String getGoodsPrice() {
+		return goodsPrice;
+	}
+	public void setGoodsPrice(String goodsPrice) {
+		this.goodsPrice = goodsPrice;
+	}
+	public String getBuynowAmount() {
+		return buynowAmount;
+	}
+	public void setBuynowAmount(String buynowAmount) {
+		this.buynowAmount = buynowAmount;
+	}
+	public String getExpectedAmount() {
+		return expectedAmount;
+	}
+	public void setExpectedAmount(String expectedAmount) {
+		this.expectedAmount = expectedAmount;
+	}
+	public String getUsePoint() {
+		return usePoint;
+	}
+	public void setUsePoint(String usePoint) {
+		this.usePoint = usePoint;
+	}
+	public String getFinalPaymentAmount() {
+		return finalPaymentAmount;
+	}
+	public void setFinalPaymentAmount(String finalPaymentAmount) {
+		this.finalPaymentAmount = finalPaymentAmount;
+	}
+	public String getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Buynow [buynowCode=");
+		builder.append(buynowCode);
+		builder.append(", userId=");
+		builder.append(userId);
+		builder.append(", goodsCode=");
+		builder.append(goodsCode);
+		builder.append(", goodsName=");
+		builder.append(goodsName);
+		builder.append(", categoryMediumCode=");
+		builder.append(categoryMediumCode);
+		builder.append(", categoryMediumName=");
+		builder.append(categoryMediumName);
+		builder.append(", goodsPrice=");
+		builder.append(goodsPrice);
+		builder.append(", buynowAmount=");
+		builder.append(buynowAmount);
+		builder.append(", expectedAmount=");
+		builder.append(expectedAmount);
+		builder.append(", usePoint=");
+		builder.append(usePoint);
+		builder.append(", finalPaymentAmount=");
+		builder.append(finalPaymentAmount);
+		builder.append(", regDate=");
+		builder.append(regDate);
+		builder.append("]");
+		return builder.toString();
+	}
+}
+
