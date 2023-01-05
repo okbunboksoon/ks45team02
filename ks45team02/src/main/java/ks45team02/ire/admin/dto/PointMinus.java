98cefb139fea1cf6f5faf92d07e6a01b663c3207
ksmart <ksmart@DESKTOP-LIJ9H19>
2023-01-05 17:13:43 +0900

    [teamproject ire] author: kmk 변경사항: PointMinus, PointSave dto 추가 변경시간: 1월 5일 17시 12분

---
+++ ks45team02/src/main/java/ks45team02/ire/admin/dto/PointMinus.java
@@ -0,0 +1,77 @@
+package ks45team02.ire.admin.dto;
+
+public class PointMinus {
+
+	private String pointMinusCode;
+	private String userId;
+	private int pointMinus;
+	private int pointState;
+	private String pointMinusDate;
+	private String pointMinusReason;
+	private String pointUseGroup;
+	
+	public String getPointMinusCode() {
+		return pointMinusCode;
+	}
+	public void setPointMinusCode(String pointMinusCode) {
+		this.pointMinusCode = pointMinusCode;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public int getPointMinus() {
+		return pointMinus;
+	}
+	public void setPointMinus(int pointMinus) {
+		this.pointMinus = pointMinus;
+	}
+	public int getPointState() {
+		return pointState;
+	}
+	public void setPointState(int pointState) {
+		this.pointState = pointState;
+	}
+	public String getPointMinusDate() {
+		return pointMinusDate;
+	}
+	public void setPointMinusDate(String pointMinusDate) {
+		this.pointMinusDate = pointMinusDate;
+	}
+	public String getPointMinusReason() {
+		return pointMinusReason;
+	}
+	public void setPointMinusReason(String pointMinusReason) {
+		this.pointMinusReason = pointMinusReason;
+	}
+	public String getPointUseGroup() {
+		return pointUseGroup;
+	}
+	public void setPointUseGroup(String pointUseGroup) {
+		this.pointUseGroup = pointUseGroup;
+	}
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("PointMinus [pointMinusCode=");
+		builder.append(pointMinusCode);
+		builder.append(", userId=");
+		builder.append(userId);
+		builder.append(", pointMinus=");
+		builder.append(pointMinus);
+		builder.append(", pointState=");
+		builder.append(pointState);
+		builder.append(", pointMinusDate=");
+		builder.append(pointMinusDate);
+		builder.append(", pointMinusReason=");
+		builder.append(pointMinusReason);
+		builder.append(", pointUseGroup=");
+		builder.append(pointUseGroup);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+}
