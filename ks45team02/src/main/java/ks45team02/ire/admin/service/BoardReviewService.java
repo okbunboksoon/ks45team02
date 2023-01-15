85ce239153dfee08798704707cb3ab8b13ae1036
kmg49 <kmg49@192.168.219.101>
2023-01-15 20:26:51 +0900

    [teamproject ire] author: kmk 변경사항: boardReview 이미지 첨부 및 출력 기능 추가, 텍스트 리뷰와 포토 리뷰 구분 변경시간: 1월 15일 18시 26분

--- ks45team02/src/main/java/ks45team02/ire/admin/service/BoardReviewService.java
+++ ks45team02/src/main/java/ks45team02/ire/admin/service/BoardReviewService.java
@@ -1,16 +1,19 @@
 package ks45team02.ire.admin.service;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import ks45team02.ire.admin.dto.BoardReview;
 import ks45team02.ire.admin.dto.PointSave;
 import ks45team02.ire.admin.dto.PointSaveStandard;
-import ks45team02.ire.admin.dto.User;
 import ks45team02.ire.admin.mapper.BoardReviewMapper;
 import ks45team02.ire.admin.mapper.PointMapper;
 import ks45team02.ire.admin.mapper.UserMapper;
@@ -33,7 +36,14 @@ public class BoardReviewService {
 		this.pointService = pointService;
 	}
 	
-	public int modifyBoardReview(BoardReview boardReview) {
+	/**
+	 * 리뷰 글 수정
+	 * @param boardReview
+	 * @return int
+	 * @throws IOException 
+	 * @throws IllegalStateException 
+	 */
+	public int modifyBoardReview(BoardReview boardReview, MultipartFile reviewImage) throws IllegalStateException, IOException {
 		
 		int result = 0;
 	
@@ -41,6 +51,25 @@ public class BoardReviewService {
 		if(idCheck == 0) {
 			return 10;
 		}
+		
+			if(!reviewImage.isEmpty()) {
+			
+			//저장될 프로젝트 경로 지정
+			String projectPath = System.getProperty("user.dir") + "/src/main/resources/static/reviewImages";
+			
+			//랜덤 식별자
+			UUID uuid = UUID.randomUUID();
+			
+			//파일 이름 생성
+			String fileName = uuid + "_" + reviewImage.getOriginalFilename();
+			
+			File saveFile = new File(projectPath, fileName);
+			
+			reviewImage.transferTo(saveFile);
+			
+			boardReview.setReviewImageName(fileName);
+			boardReview.setReviewImagePath("/reviewImages/" + fileName);
+		 }
 		
 		result = boardReviewMapper.modifyBoardReview(boardReview);
 		
@@ -51,8 +80,10 @@ public class BoardReviewService {
 	 * 리뷰 글 등록
 	 * @param boardReview
 	 * @return int
+	 * @throws IOException 
+	 * @throws IllegalStateException 
 	 */
-	public int addBoardReview(BoardReview boardReview) {
+	public int addBoardReview(BoardReview boardReview, MultipartFile reviewImage) throws IllegalStateException, IOException {
 		
 		int result = 0;
 		
@@ -61,24 +92,53 @@ public class BoardReviewService {
 			return 10;
 		}
 		
-		//텍스트 리뷰인지 포토 리뷰인지 조건 추가 후 포인트 적립금 및 포인트 그룹 구분 필요
+		if(!reviewImage.isEmpty()) {
+			
+			//저장될 프로젝트 경로 지정
+			String projectPath = System.getProperty("user.dir") + "/src/main/resources/static/reviewImages";
+			
+			//랜덤 식별자
+			UUID uuid = UUID.randomUUID();
+			
+			//파일 이름 생성
+			String fileName = uuid + "_" + reviewImage.getOriginalFilename();
+			
+			File saveFile = new File(projectPath, fileName);
+			
+			reviewImage.transferTo(saveFile);
+			
+			boardReview.setReviewImageName(fileName);
+			boardReview.setReviewImagePath("/reviewImages/" + fileName);
+		 }
+		
 		int pointSaveAmount = 0;
-		if(true) {
+		PointSave pointSave = new PointSave();
+		if(reviewImage.isEmpty()) {
 			//텍스트 리뷰 적립금
 			PointSaveStandard pointSaveStandardInfo = pointMapper.getPointSaveStandardInfo("point_save_standard_002");
 			pointSaveAmount = pointSaveStandardInfo.getPointSave();
 			boardReview.setReviewPointSave(pointSaveAmount);
 			
+			pointSave.setPointSaveReason("텍스트 리뷰 등록");
+			
 			//포인트 그룹 추가
 			boardReview.setReviewPointGroup("text_review");
+		}else {
+			//포토 리뷰 적립금
+			PointSaveStandard pointSaveStandardInfo = pointMapper.getPointSaveStandardInfo("point_save_standard_003");
+			pointSaveAmount = pointSaveStandardInfo.getPointSave();
+			boardReview.setReviewPointSave(pointSaveAmount);
+			
+			pointSave.setPointSaveReason("포토 리뷰 등록");
+			
+			//포인트 그룹 추가
+			boardReview.setReviewPointGroup("photo_review");
 		}
 		result = boardReviewMapper.addBoardReview(boardReview);
 		
 		//포인트 적립 처리
-		PointSave pointSave = new PointSave();
 		pointSave.setUserId(boardReview.getUserId());
 		pointSave.setPointSave(pointSaveAmount);
-		pointSave.setPointSaveReason("텍스트 리뷰 등록");
 		pointSave.setPointSaveGroup(boardReview.getReviewPointGroup());
 		pointService.addPointSave(pointSave);
 		
