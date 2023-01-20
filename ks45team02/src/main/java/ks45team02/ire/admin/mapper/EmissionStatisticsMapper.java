3d6883b2922f2c775187110392ee6079fb5f7674
min <min@DESKTOP-C5CNQU2>
2023-01-20 16:09:49 +0900

    [teamproject ire] author: ydh 변경사항: 기부받은 의류 co2통계 일 월 년 조회 및 등록 업데이트 변경시간: 1월 20일 16시

--- ks45team02/src/main/java/ks45team02/ire/admin/mapper/EmissionStatisticsMapper.java
+++ ks45team02/src/main/java/ks45team02/ire/admin/mapper/EmissionStatisticsMapper.java
@@ -14,28 +14,37 @@ public interface EmissionStatisticsMapper {
 	/**
 	 * 기부받은 의류별 CO2 일별 전체 조회
 	 */
-	public List<RawMaterialsEmissionStatisticsDay> rawMaterialsDaySearch();
+	public List<RawMaterialsEmissionStatisticsDay>  rawMaterialsDaySearch();
 	
 	/**
-	 * 기부받은 의류별 CO2 월별 전체 조회
+	 * 기부받은 의류별 CO2 일별 전체 조회
 	 */
-	public List<RawMaterialsEmissionStatisticsMonth> rawMaterialsMonthSearch();
+	public List<RawMaterialsEmissionStatisticsMonth>  rawMaterialsMonthSearch();
 	
 	/**
-	 * 기부받은 의류별 CO2 년별 전체 조회
+	 * 기부받은 의류별 CO2 일별 전체 조회
 	 */
-	public List<RawMaterialsEmissionStatisticsYear> rawMaterialsYearhSearch();
+	public List<RawMaterialsEmissionStatisticsYear>  rawMaterialsYearhSearch();
+	
+	/**
+	 * 기부받은 의류별 CO2 등록날짜
+	 */
+	public String getRawMaterialsIncomingDate(String rawMaterialsCode);
+	
 	/**
 	 * 기부받은 의류별 CO2 일별 통계 추가
 	 */
 	public int rawMaterialsDayAdd (RawMaterialsEmissionStatisticsDay rawMaterialsEmissionStatisticsDay);
-	
 	
 	/**
 	 * CO2 통계 입고 그룹코드 존재 여부 확인
 	 */
 	public int rawMaterialsDayCheck (String rawMaterialsDaygroup);
 	
+	/**
+	 * co2 통계 월별 그룹코드 존재확인
+	 */
+	public int rawMaterialsMonthCheck (String monthCode);
 	/**
 	 * 기부받은 의류별 CO2 일별 일반 상품별 이산화탄소 배출량 조회
 	 */
@@ -51,11 +60,103 @@ public interface EmissionStatisticsMapper {
 	 */
 	public int rawMaterialsDayUpdate(RawMaterialsEmissionStatisticsDay rawMaterialsEmissionStatisticsDay);
 
-	
-	
-	
+	/**
+	 * 월별 등록 이산화탄소 합계
+	 */
+	public int getMonthEmissionSum(String monthCode);
 	
 	/**
-	 * 
+	 * 월별 등록
 	 */
+	public int addrawMaterialsMonth(RawMaterialsEmissionStatisticsMonth rawMaterialsEmissionStatisticsMonth);
+	
+	/**
+	 * 월별 업데이트
+	 */
+	public int updaterawMaterialsMonth(RawMaterialsEmissionStatisticsMonth rawMaterialsEmissionStatisticsMonth);
+
+	/**
+	 * 년별 존재 확인
+	 */
+	public int rawMaterialsYearCheck(String yearCode);
+	
+	/**
+	 * 년별 year 추출
+	 */
+	public String getYear(String yearCode);
+	
+	/**
+	 * 년별 month 추출
+	 */
+	public String getMonth(String yearCode);
+	
+	/**
+	 * 년별 등록
+	 */
+	public int addrawMaterialsYear(RawMaterialsEmissionStatisticsYear rawMaterialsEmissionStatisticsYear);
+	
+	/**
+	 * 년별 월 코드 합계 추출
+	 */
+	public int getEmissionSum(String yearCode);
+	
+	/**
+	 * 1월 업데이트
+	 */
+	public int updateJan(RawMaterialsEmissionStatisticsYear rawMaterialsEmissionStatisticsYear);
+	
+	/**
+	 * 2월 업데이트
+	 */
+	public int updateFeb(RawMaterialsEmissionStatisticsYear rawMaterialsEmissionStatisticsYear);
+	
+	/**
+	 * 3월 업데이트
+	 */
+	public int updateMar(RawMaterialsEmissionStatisticsYear rawMaterialsEmissionStatisticsYear);
+	
+	/**
+	 * 4월 업데이트
+	 */
+	public int updateApr(RawMaterialsEmissionStatisticsYear rawMaterialsEmissionStatisticsYear);
+	
+	/**
+	 * 5월 업데이트
+	 */
+	public int updateMay(RawMaterialsEmissionStatisticsYear rawMaterialsEmissionStatisticsYear);
+	
+	/**
+	 * 6월 업데이트
+	 */
+	public int updateJun(RawMaterialsEmissionStatisticsYear rawMaterialsEmissionStatisticsYear);
+	
+	/**
+	 * 7월 업데이트
+	 */
+	public int updateJul(RawMaterialsEmissionStatisticsYear rawMaterialsEmissionStatisticsYear);
+	
+	/**
+	 * 8월 업데이트
+	 */
+	public int updateAug(RawMaterialsEmissionStatisticsYear rawMaterialsEmissionStatisticsYear);
+	
+	/**
+	 * 9월 업데이트
+	 */
+	public int updateSept(RawMaterialsEmissionStatisticsYear rawMaterialsEmissionStatisticsYear);
+	
+	/**
+	 * 10월 업데이트
+	 */
+	public int updateOct(RawMaterialsEmissionStatisticsYear rawMaterialsEmissionStatisticsYear);
+	
+	/**
+	 * 11월 업데이트
+	 */
+	public int updateNov(RawMaterialsEmissionStatisticsYear rawMaterialsEmissionStatisticsYear);
+	
+	/**
+	 * 12월 업데이트
+	 */
+	public int updateDec(RawMaterialsEmissionStatisticsYear rawMaterialsEmissionStatisticsYear);
 }
