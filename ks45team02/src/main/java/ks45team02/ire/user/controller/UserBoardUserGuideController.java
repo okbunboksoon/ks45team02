84dbf6aa70691a3f18fac2794b31a0544e2a4b63
cossw <cossw@DESKTOP-UI7EGP1>
2023-01-23 10:54:59 +0900

    이용가이드, event 배너 이미지 수정

--- ks45team02/src/main/java/ks45team02/ire/user/controller/UserBoardUserGuideController.java
+++ ks45team02/src/main/java/ks45team02/ire/user/controller/UserBoardUserGuideController.java
@@ -1,6 +1,7 @@
 package ks45team02.ire.user.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,58 +13,60 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import ks45team02.ire.admin.dto.BoardUserGuide;
 import ks45team02.ire.admin.service.BoardUserGuideService;
-
+import ks45team02.ire.user.service.UserBoardUserGuideService;
 
 @Controller
 @RequestMapping("/")
 public class UserBoardUserGuideController {
 
-	
 	private static final Logger log = LoggerFactory.getLogger(UserBoardUserGuideController.class);
 
-	private final BoardUserGuideService boardUserGuideService;
-	
-	public UserBoardUserGuideController(BoardUserGuideService boardUserGuideService) {
-		this.boardUserGuideService = boardUserGuideService;
+	private final UserBoardUserGuideService userBoardUserGuideService;
+
+	public UserBoardUserGuideController(UserBoardUserGuideService userBoardUserGuideService) {
+		this.userBoardUserGuideService = userBoardUserGuideService;
 	}
-	
-	
+
 	/**
 	 * 이용가이드 목록
+	 * 
 	 * @param model
 	 */
-	@GetMapping("/listBoardUserGuide")
-	public String listBoardUserGuide(Model model) {
+	
+	  @GetMapping("/listBoardUserGuide")
+	  
+	  public String listBoardUserGuide(Model model)  {
+	  
+	  List <BoardUserGuide> userGuideList = userBoardUserGuideService.getUserGuideList();
+	  
+	  model.addAttribute("title", "Ire 이용가이드"); 
+	  model.addAttribute("userGuideList",userGuideList); 
 
-		List<BoardUserGuide> userGuideList = boardUserGuideService.getUserGuideList();
-		
-		model.addAttribute("title", "Ire 이용가이드");
-		model.addAttribute("userGuideList", userGuideList);
-		
-		return "user/board/boardListUserGuide";
-	}
-	
-	
-	
-	/** 
-	 * 상세페이지
-	 * @param guide_code model
-	 * @return user/board/boardContentsUserGuide
-	 */
-	@GetMapping("/ContentsUserGuide")
-	public String ContentsUserGuide (@RequestParam(value = "guide_code") String guide_code, Model model) throws Exception {
-		
-		boardUserGuideService.updateViewsCnt(guide_code);
-		
-		List<BoardUserGuide> contentsUserGuide = boardUserGuideService.ContentsUserGuide(guide_code);
-		model.addAttribute("title", "이용가이드 상세페이지");
-		model.addAttribute("pageTitle", "이용가이드 상세페이지");
-		model.addAttribute("contentsUserGuide", contentsUserGuide);
-		
-		return "user/board/boardContentsUserGuide";
-		
-	}
+	  
+	  return "user/board/boardListUserGuide"; }
+	 
 
+		
+		/**
+			 * 상세페이지
+			 * 
+			 * @param guide_code model
+			 * @return user/board/boardContentsUserGuide
+			 */
+				  @GetMapping("/ContentsUserGuide") public String ContentsUserGuide
+				  (@RequestParam(value = "guide_code") String guide_code, Model model) throws
+				  Exception {
+				  
+				  userBoardUserGuideService.updateViewsCnt(guide_code);
+				  
+				  List<BoardUserGuide> contentsUserGuide =
+				  userBoardUserGuideService.ContentsUserGuide(guide_code);
+				  model.addAttribute("title", "이용가이드 상세페이지"); model.addAttribute("pageTitle",
+				  "이용가이드 상세페이지"); model.addAttribute("contentsUserGuide", contentsUserGuide);
+				  
+				  return "user/board/boardContentsUserGuide";
+				  
+				  }
+				 
 
 }
-
